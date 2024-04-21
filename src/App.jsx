cd0e067b710f690164d30c33f70de163f4575314
jonnek <jonnekoivisto00@gmail.com>
2024-04-21 16:36:53 +0300

    app build

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+
 const App = () => {
   return (
-      <>
-        <h1>My App</h1>
-        <Home />
-      </>
+      <Router basename={import.meta.env.BASE_URL}>
+        <>
+          <h1>My App</h1>
+          <Home />
+        </>
+      </Router>
   );
 };
+
 export default App;
